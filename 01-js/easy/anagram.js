8b689717a26eeab839240d877de6aa06fa8af41b
Ayush999Tiwari <149681041+Ayush999Tiwari@users.noreply.github.com>
2025-03-27 13:41:11 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,19 @@
 */
 
 function isAnagram(str1, str2) {
-
-}
+  str1 = sortString(str1.tolowercase());  // characers of both strings are sorted in aphabetical order
+  str2=  sortString(str2.tolowercase());  // a sort string function is decleard
+  // now comparision of both striings should be done
+  if(str1==str2){
+    return true;
+}else{
+    rteurn false;
+  }
+  function sortString(str){
+    let char=str.split(''); //split function is inbuiltjs function which split characters of any string
+    char.sort();
+    let ans= char.join(''); 
+    retru  ans;
+  )
 
 module.exports = isAnagram;
